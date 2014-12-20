81b5c5c29747d7f8622828c0036504a4a5023794
Alexander Bauer <sasha@crofter.org>
2014-12-20 14:36:35 -0500

    Develop the Regex grammar slightly further

--- parse-demo.py
+++ parse-demo.py
@@ -14,15 +14,17 @@ with open(filename, 'r') as f:
 
 # Break the input down into sentences, then into words, and position tag
 # those words.
+raw_sentences = nltk.sent_tokenize(data)
 sentences = [nltk.pos_tag(nltk.word_tokenize(sentence)) \
-    for sentence in nltk.sent_tokenize(data)]
+    for sentence in raw_sentences]
 
 # Define a grammar, and identify the noun phrases in the sentences.
-chunk_parser = nltk.RegexpParser(r"NP: {<DT>?<JJ>*<NN>}")
+chunk_parser = nltk.RegexpParser(r"NP: {((<DT>|<PRP\$>)?<JJ>*(<NN>|<NNP>)+|<PRP>)}")
 
 trees = [chunk_parser.parse(sentence) for sentence in sentences]
 
-for tree in trees:
-    print(tree)
-    #for subtree in tree.subtrees(filter = lambda t: t.label() == 'NP'):
-        #print(subtree)
+for index, tree in enumerate(trees):
+    print("===\nSentence: %s\nNoun phrases:" %
+            raw_sentences[index].replace('\n', ' '))
+    for subtree in tree.subtrees(filter = lambda t: t.label() == 'NP'):
+        print("  %s" % subtree)
